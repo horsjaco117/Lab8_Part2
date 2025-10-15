b2701d9a534b8cb73ae08f41349d759ff2fdf361
horsjaco117 <jacob_horsley@live.com>
2025-10-15 13:44:28 -0600

    Commit of code from lab8 working ADC.

---
+++ Lab8Servo.X/Lab8Servo.s
@@ -0,0 +1,161 @@
+
+; LAB 8
+; Jacob Horsley
+; RCET
+; Fifth Semester
+; ADC and lights
+; Device: PIC16F883
+;GITHUB: https://github.com/horsjaco117/Lab8-ADC-Servo-Control
+;-------------------------------------------------------------------------------
+; Configuration
+    ; CONFIG1
+    CONFIG FOSC = XT ; Oscillator Selection bits (XT oscillator)
+    CONFIG WDTE = OFF ; Watchdog Timer Enable bit (WDT disabled)
+    CONFIG PWRTE = OFF ; Power-up Timer Enable bit (PWRT disabled)
+    CONFIG MCLRE = ON ; RE3/MCLR pin function select bit (MCLR)
+    CONFIG CP = OFF ; Code Protection bit (Program memory code protection disabled)
+    CONFIG CPD = OFF ; Data Code Protection bit (Data memory code protection disabled)
+    CONFIG BOREN = OFF ; Brown Out Reset Selection bits (BOR disabled)
+    CONFIG IESO = OFF ; Internal External Switchover bit (disabled)
+    CONFIG FCMEN = OFF ; Fail-Safe Clock Monitor Enabled bit (disabled)
+    CONFIG LVP = OFF ; Low Voltage Programming Enable bit (RB3 is digital I/O)
+    ; CONFIG2
+    CONFIG BOR4V = BOR40V ; Brown-out Reset Selection bit (set to 4.0V)
+    CONFIG WRT = OFF ; Flash Program Memory Self Write Enable bits (Write protection off)
+   
+; Include Statements
+#include <xc.inc>
+   
+; Code Section
+;------------------------------------------------------------------------------
+; Register/Variable Setup
+    RESULT_HI EQU 0X21	      ;For ADC High Byte
+    RESULT_LO EQU 0X22
+    W_TEMP EQU 0X23
+    STATUS_TEMP EQU 0X24
+ 
+; Start of Program
+; Reset vector address
+    PSECT resetVect, class=CODE, delta=2
+    GOTO Start
+; Interrupt vector
+    PSECT isrVect, class=CODE, delta=2 ;Jumps to the interrupt that handles TMR2
+    GOTO INTERRUPT
+; Setup Code
+    PSECT code, class=CODE, delta=2
+Start:
+Setup:
+    ; Bank 3 (ANSELH, ANSEL)
+    BSF STATUS, 5 ; Go to Bank 3
+    BSF STATUS, 6
+    CLRF ANSELH ; Set pins to digital I/O
+    MOVLW 0X02 ;Pin 3 selected
+    MOVWF ANSEL ;Sets Pins to analog
+   
+    ; Bank 2 (CM2CON0, CM2CON1)
+    BSF STATUS, 5
+    BCF STATUS, 6 ; Go to Bank 2
+    CLRF CM2CON1 ; Disable Comparator 2
+    CLRF CM2CON0 ; Disable Comparator 1
+   
+    ; Bank 1 (TRISB, WPUB, IOCB, OPTION_REG, TRISC, TRISA, PIE1, PR2)
+    BSF STATUS, 5
+    BCF STATUS, 6 ; Go to Bank 1
+    MOVLW 0x00 ; PortB as Outputs
+    MOVWF TRISB
+    MOVLW 0x00 ; Disable weak pull-ups on Port B
+    MOVWF WPUB
+    MOVLW 0x00 ; Disable interrupt-on-change for RB0/RB1
+    MOVWF IOCB
+    CLRF OPTION_REG ; Enable global pull-ups, clear Timer0 settings
+    MOVLW 0X00
+    MOVWF TRISC ; Port C as outputs (for traffic lights)
+    MOVLW 0x03 ; RA0 as an input
+    MOVWF TRISA ; Set RA0, RA1 as inputs for buttons
+    MOVLW 0x42 ; ADIE enabled, timer 2 enabled too
+    MOVWF PIE1
+    MOVLW 0xF0 ; Timer2 period set
+    MOVWF PR2
+    MOVLW 0XC5 ;Right adjust
+    MOVWF ADCON1
+    BSF ADCON1, 7
+    
+    ; Bank 0 (INTCON, ports, peripherals)
+    BCF STATUS, 5 ; Go to Bank 0
+    BCF STATUS, 6
+
+    MOVLW 0XC5 ;AN1 pin selected, ADON=1, ADCS=01
+    MOVWF ADCON0
+    MOVLW 0x00 ; Interrupts disabled initially
+    MOVWF INTCON
+    MOVLW 0X00  ;Clear peripheral interrupt flags
+    MOVWF PIR1 
+    MOVLW 0XC0 ; Enable GIE and PEIE
+    MOVWF INTCON
+    CLRF CCP1CON ; Disable PWM
+    MOVLW 0X00
+    MOVWF PORTB ; Clear Port B
+    CLRF CCP2CON ; Disable second PWM
+    CLRF PORTA ; Clear Port A
+    CLRF PORTC ; Clear Port C
+    CLRF RCSTA ; Disable serial control
+    CLRF SSPCON ; Disable serial port
+    CLRF T1CON ; Disable Timer1
+    CLRF PSTRCON ; Disable PWM pulse steering
+    CLRF CM2CON1 ; Disable Comparator 2
+    MOVLW 0x00
+    MOVWF TMR2 ; Reset Timer2
+    MOVLW 0x05 ; Prescaler 1:4, TMR2ON=1
+    MOVWF T2CON
+    BSF ADCON0, 1 ; Start initial ADC conversion
+
+    BANKSEL ADCON1 ;Ensures the code is right justified to avoid the lsb
+    BCF ADCON1, 7
+   
+; Main Program Loop
+MAINLOOP:
+    ; Optional: Toggle PORTC for testing (add delay if needed)
+    MOVF PORTC,0	;Read current value of PortC into register(W)
+    XORLW 0X05		;XOR the working register with 0x05
+    MOVWF PORTC		;Outputs the value from register to outputs (PortC)
+    ; CALL  DELAY		;Delay of ~.5 seconds called (define subroutine if used)
+    
+    GOTO  MAINLOOP	;Repeats indefinitely
+    
+
+INTERRUPT:
+    MOVWF W_TEMP	;Saves all the data from mainloop
+    SWAPF STATUS, W
+    MOVWF STATUS_TEMP
+    
+    BTFSS   PIR1, 6     ; Check ADIF
+    GOTO CHECK_OTHER
+    BCF PIR1, 6         ; Clear ADIF
+    BANKSEL ADRESH      ; Ensure Bank 0 for ADRESH
+    MOVF ADRESH, W
+    MOVWF RESULT_HI
+    BANKSEL ADRESL      ; Bank 1 for ADRESL
+    MOVF ADRESL, W
+    BANKSEL RESULT_LO   ; Back to Bank 0
+    MOVWF RESULT_LO
+    
+    MOVF RESULT_HI, W   ; Example: Output low byte to PORTC for testing
+    MOVWF PORTB
+    
+CHECK_OTHER:
+    BTFSS PIR1, 1       ; Check TMR2IF
+    GOTO EXIT_ISR
+    BCF PIR1,1          ; Clear TMR2IF
+    BSF ADCON0, 1       ; Start next ADC conversion
+  
+    
+EXIT_ISR:
+    SWAPF STATUS_TEMP, W;Loads all the data from the mainloop
+    MOVWF STATUS
+    SWAPF W_TEMP, F
+    SWAPF W_TEMP, W
+    RETFIE
+    
+    
+END
+
